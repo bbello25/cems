c3f2503f746bf06a3456a137386285a64593ad75
Branislav <branislav.bellovic@sde.cz>
2018-11-23 19:11:58 +0100

    Environemnts bug fixes

--- cems-SPA/src/environments/environment.prod.ts
+++ cems-SPA/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://cems.sde.cz:5443/api/',
   whitelistedDomain: 'cems.sde.cz:5443',
-  blacklistedRoutes: 'cems.sde.cz:5443//api/auth'
+  blacklistedRoute: 'cems.sde.cz:5443//api/auth'
 };
